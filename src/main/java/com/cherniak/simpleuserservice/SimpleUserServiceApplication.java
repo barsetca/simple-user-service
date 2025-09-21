576a74d57afd0162c791b641b6555cd45e871860
chernyak <chernyak@protei-lab.ru>
2025-09-21 12:27:05 +0200

    initial commit

---
+++ src/main/java/com/cherniak/simpleuserservice/SimpleUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cherniak.simpleuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleUserServiceApplication.class, args);
+	}
+
+}
